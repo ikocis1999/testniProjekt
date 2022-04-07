d95e73453662f6b7b15a66672f60eea988f7df9a
astudent <astudent@DESKTOP-GV9CUPH>
2022-04-07 15:35:34 +0200

    Prva poruka

---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module ATA_LV2 {
+}
---
+++ src/testProject/testnaKlasa.java
@@ -0,0 +1,10 @@
+package testProject;
+
+public class testnaKlasa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testiranje instalacije je uspjesno!");
+	}
+
+}
